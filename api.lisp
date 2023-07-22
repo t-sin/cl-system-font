143b3ad6d19e3855aad29407f6c32777429a1957
t-sin <shinichi.tanaka45@gmail.com>
2023-07-22 22:54:00 +0900

    Enable cached font info list

--- api.lisp
+++ api.lisp
@@ -1,5 +1,11 @@
 (in-package :system-font)
 
-(defun font-info-list ()
-  #+linux
-  (font-info-list.linux))
+(defparameter *font-info-list-cache* nil)
+
+(defun font-info-list (&key cache)
+  (when (and *font-info-list-cache* cache)
+    *font-info-list-cache*)
+  (let ((font-info-list #+linux
+                        (font-info-list.linux)))
+    (prog1 font-info-list
+      (setf *font-info-list-cache* font-info-list))))
